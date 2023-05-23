ae5c1d9222faca94b08e2e69ada7aac03cce96f2
Filippo Vicidomini <filippovicidomini@gmail.com>
2023-05-23 16:18:56 +0200

    si può fare facilmente

--- main.py
+++ main.py
@@ -1,1 +1,24 @@
 #prova
+# open a file txt and read it
+# return a list of words
+file = open("example.txt", "r")
+words = file.read().split()
+file.close()
+
+print(words)
+
+e_file = []
+for word in words:
+    if word[0] == "E":
+        e_file.append(word)
+        words.remove(word)
+print(words)
+print(e_file)
+
+#create a file txt and write it with the list of words that start with E separatete by endline
+file = open("example2.txt", "w")
+for word in e_file:
+    file.write(word + "\n")
+file.close()
+
+
